041cda5645c2afe4c90f507da9dc9d61eb022de5
Sondre Johnsen <sjohnsen.valle@gmail.com>
2021-11-09 21:02:32 +0100

    Updated pole cart model

--- Source/InvertedPendulum/Private/Physics/NumericalAnalysis.cpp
+++ Source/InvertedPendulum/Private/Physics/NumericalAnalysis.cpp
@@ -18,7 +18,7 @@ bool FNumericalAnalysis::Init()
 	m_pole = 0.100;  // 0.071
 	m_cart = 0.200;  // 0.288
 	l = (0.685 - 0.246);
-	theta = 2.85;
+	theta = 3.12;
 	current_theta = theta;
 	theta_d = 0;
 	theta_dd = 0;
@@ -41,11 +41,17 @@ uint32 FNumericalAnalysis::Run()
 	std::chrono::steady_clock::time_point step_end_time;
 	std::chrono::steady_clock::time_point loop_end_time;
 
+	float u = 0;
+	float d = 0;
 	while (!stop_thread) {		
 		step_start_time = std::chrono::steady_clock::now();
 
-		theta_dd = (- g * sin(theta) - pos_dd * cos(theta))/l;
-		pos_dd = (-m_pole * l * theta_dd * cos(theta) + m_pole * l * theta_d * theta_d * sin(theta)) / (m_cart + m_pole);
+		// theta_dd = (- g * sin(theta) - pos_dd * cos(theta))/l;
+		// pos_dd = (-m_pole * l * theta_dd * cos(theta) + m_pole * l * theta_d * theta_d * sin(theta)) / (m_cart + m_pole);
+
+		// Advance model
+		pos_dd = (m_pole*m_pole*l*l*g*cos(theta)*sin(theta) + m_pole*l*l*(m_pole*l*theta_d*theta_d*sin(theta) - d*pos_d) + m_pole*l*l*u) / (m_pole*l*l* (m_cart + m_pole*(1-cos(theta)*cos(theta))));
+		theta_dd = (-(m_pole+m_cart)*m_pole*g*l*sin(theta) - m_pole*l*cos(theta)*(m_pole*l*theta_d*theta_d*sin(theta)- d*pos_d) + m_pole*l*cos(theta)*u) / (m_pole*l*l*(m_cart+m_pole*(1-cos(theta)*cos(theta))));
 
 		theta_d = theta_dd * dt + theta_d;
 		pos_d = pos_dd * dt + pos_d;
@@ -79,7 +85,7 @@ uint32 FNumericalAnalysis::Run()
 			UE_LOG(LogTemp, Warning, TEXT("Elapsed Time: %f[s], Step Time: %f[s]"),loop_period, step_period);
 			step_count = 0;
 		}
-
+	// TODO fool loop is dt + 0.0005 might be better to only check the total time and end.
 	}
 	return 0;
 }
